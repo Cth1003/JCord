f96c9511b505305b5bfd3e072e173fd2ffcd51d1
Cth103 <ItsAMeCth@gmail.com>
2016-10-21 12:49:00 +0100

    Added a definition of loginWithToken

--- src/main/java/com/TheUltimatePleb/JCord/util/IDiscordAPI.java
+++ src/main/java/com/TheUltimatePleb/JCord/util/IDiscordAPI.java
@@ -1,11 +1,20 @@
 package com.TheUltimatePleb.JCord.util;
 
+import com.TheUltimatePleb.JCord.util.WebUtils;
+
 public class IDiscordAPI implements com.TheUltimatePleb.JCord.DiscordAPI {
 
-    @Override
+   @Override
     public void loginWithToken(String token, boolean isBot) {
-
+    JSONObject login = postWithHeader("auth/login", "token", token);
+	 if (!login == null){ 
+        throw "Token is nothing.";
+    } else {
+		//Connected.
+		}
     }
+    }
+
 
     @Override
     public void loginAsUser(String email, String password) {
