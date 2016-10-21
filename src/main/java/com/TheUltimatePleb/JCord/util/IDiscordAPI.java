f40193a18f33a737f4aaa1bd653fbb83b9a18b78
Cth103 <ItsAMeCth@gmail.com>
2016-10-21 12:53:14 +0100

    Updated loginWithToken

--- src/main/java/com/TheUltimatePleb/JCord/util/IDiscordAPI.java
+++ src/main/java/com/TheUltimatePleb/JCord/util/IDiscordAPI.java
@@ -4,7 +4,13 @@ public class IDiscordAPI implements com.TheUltimatePleb.JCord.DiscordAPI {
 
     @Override
     public void loginWithToken(String token, boolean isBot) {
-
+    JSONObject login = postWithHeader("auth/login", "token", token);
+	 if (login == null || log.isEmpty()){ 
+        throw "Token is nothing.";
+    } else {
+		//Connected.
+		}
+    }
     }
 
     @Override
